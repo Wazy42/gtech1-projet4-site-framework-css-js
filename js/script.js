f8fb08a728d78bf847b1a680582a047757d27c4a
SmartNewt <mateescuraphael@icloud.com>
2021-11-18 15:03:45 +0100

    ajout formulaire contact et images carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+$('.carousel').carousel({
+    fullWidth: true
+});
+
 $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: false
@@ -14,3 +18,18 @@ $('.movePrevCarousel').click(function(e){
     e.stopPropagation();
     $('.carousel').carousel('prev');
 });
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
