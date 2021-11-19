7e8d7cb19f7c6113eeba05e40214f79fcccc5e21
Wazy42 <lvocanson@gaming.tech>
2021-11-19 08:45:08 +0100

    Formulaire navbar + title

--- js/script.js
+++ js/script.js
@@ -23,13 +23,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
+$(document).ready(function(){
     $('.modal').modal();
-  });
+});
